32641cda6b6437a9242c709e8c1e91ce1acb987c
Isabella <angelcuartas18@gmail.com>
2025-04-09 17:09:22 -0500

    Ahora si funcionan había exportado mal

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -2,23 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home/Home';
-import Bristo from './pages/Cafeteria Bristo/Bristo';
-import CafeteriaDetail from './pages/Cafeteria Detail/CafeteriaDetail';
-//import './App.css';
+import CafeteriaDetail from './components/CafeteriaDetail/CafeteriaDetail';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-        
+  return (
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<Login />} />
-        <Route path ="/signup" element={<Signup />} />
-		<Route path ="/home" element={<Home />} />
-		<Route path ="/home/bristo" element={<Bristo />} />
-		<Route path="/cafeteria/:id" element={<CafeteriaDetail />} />
-			</Routes>
-		</BrowserRouter>
-	);
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cafeteria/:id" element={<CafeteriaDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
