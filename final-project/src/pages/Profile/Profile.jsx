00fabc60aeb4ed340cfa35b8c7c8aa286916ef72
Isabella <angelcuartas18@gmail.com>
2025-04-10 11:29:12 -0500

    mala mia esta importación no iba

--- final-project/src/pages/Profile/Profile.jsx
+++ final-project/src/pages/Profile/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Login from '../Login/Login';
 import './Profile.css'; // Archivo de estilos
 
 const Profile = () => {
