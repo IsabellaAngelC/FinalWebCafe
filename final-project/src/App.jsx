650ddb26149db213ad88811029e2ef3eac4c4c53
Isabella <angelcuartas18@gmail.com>
2025-04-10 11:20:10 -0500

    aqui tratando de conectar el perfil a la navbar

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home/Home';
 import CafeteriaDetail from './components/CafeteriaDetail/CafeteriaDetail';
+import Profile from './pages/Perfil/Perfil';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/cafeteria/:id" element={<CafeteriaDetail />} />
+		<Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
